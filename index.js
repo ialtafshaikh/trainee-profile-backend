7cee00638ae9a8c7b15212b409983df3ca804a65
saurav <sourav.upadhyay@raweng.com>
2020-12-29 07:17:08 +0530

    first commit
    express added

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({
+	path: "./config.env",
+});
+
+mongoose.connect(
+	process.env.DB_URL,
+	{
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (err) {
+			console.log(err);
+		}
+		console.log("Successful");
+	}
+);
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server running on PORT no. ${process.env.PORT}`);
+});
