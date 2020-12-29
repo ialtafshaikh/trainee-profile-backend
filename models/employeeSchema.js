25b9af2167a8759b85988c7593541a063c853c5e
Rishikesh46 <rishikesh.raut@raweng.com>
2020-12-29 13:35:29 +0530

    modified schema

--- models/employeeSchema.js
+++ models/employeeSchema.js
@@ -11,62 +11,36 @@ const employeeSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: [true, "Please enter first name"],
-    validate: {
-      validator: function (firstName) {
-        return this.firstName.trim().length;
-      },
-      message: "first name should not be empty",
-    },
+   
   },
   lastName: {
     type: String,
     required: [true, "Please enter last name"],
-    validate: {
-      validator: function (lastName) {
-        return this.lastName.trim().length;
-      },
-      message: "last name should not be empty",
-    },
+    
   },
   company: {
     type: String,
     required: [true, "Please enter your company. "],
-    validate: {
-      validator: function (company) {
-        return this.company.trim().length;
-      },
-      message: "company should not be empty",
-    },
+    
   },
   email: {
     type: String,
     required: [true, "Please enter your email. "],
-    validate: {
-      validator: function (email) {
-        return this.email.trim().length;
-      },
-      message: "email should not be empty",
-    },
+    
   },
   contact: {
     type: String,
-    required: [true, "Please enter your contact number. "],
-    validate: {
-      validator: function (contact) {
-        return this.contact.trim().length;
-      },
-      message: "contact number should not be empty",
-    },
+    default:"",
+    
   },
   imageUrl: {
     type: String,
-    required: false,
     default: "",
   },
   skills: [],
   altDescription: {
     type: String,
-    required: false,
+   default:"alt-image-description",
   },
   hobbies: [],
   socialLinks: [],
