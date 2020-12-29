12988d97b55bade402096d4b9d56034ce26d07ec
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-29 11:27:40 +0530

    mongo connection updated

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config({
-	path: "./config.env",
+  path: "./config.env",
 });
 
 mongoose.connect(
-	process.env.DB_URL,
-	{
-		useCreateIndex: true,
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	},
-	(err) => {
-		if (err) {
-			console.log(err);
-		}
-		console.log("Successful");
-	}
+  process.env.DATABASE_URL,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("Successful");
+  }
 );
 
 const app = express();
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server running on PORT no. ${process.env.PORT}`);
+  console.log(`Server running on PORT no. ${process.env.PORT}`);
 });
