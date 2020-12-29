a223607807f0d79756fb6963abb448a01d0c8576
Trainee 18 <sagar.vartak@contentstack.com>
2020-12-29 13:56:41 +0530

    added route for geting employee by employee id

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { getAllemployees } = require("../controllers/employeeController");
-const {addEmployees} =require("../controllers/addEmployees")
+const { addEmployees } = require("../controllers/addEmployees");
+const { getEmployeebyId } = require("../controllers/getEmployeebyId");
 
 const router = express.Router();
 
 router.route("/").get(getAllemployees);
 router.route("/addEmployee").post(addEmployees);
+router.route("/:empId").get(getEmployeebyId);
 
 module.exports = router;
