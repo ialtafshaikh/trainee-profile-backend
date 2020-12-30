d4957e94798ed209cd17e9bdf5834bc482865c35
saurav <sourav.upadhyay@raweng.com>
2020-12-30 19:41:43 +0530

    console added

--- controllers/addEmployees.js
+++ controllers/addEmployees.js
@@ -5,21 +5,20 @@ const AppError = require("../helpers/appErrorClass");
 const sendErrorMessage = require("../helpers/sendError");
 const sendResponse = require("../helpers/sendResponse");
 
-
 const addEmployees = (req, res, next) => {
-     
-      new Employee(req.body)
-        .save()
-        .then((data) => {
-            sendResponse(200, "Employee added Succesfully", data ,req, res);
-        })
-        .catch((err) => {
-            return sendErrorMessage(
-                new AppError(400, "unsuccessful", "request body is inavlid"),
-                req,
-                res
-              );
-        });
-    }
+	console.log(req.body);
+	new Employee(req.body)
+		.save()
+		.then((data) => {
+			sendResponse(200, "Employee added Succesfully", data, req, res);
+		})
+		.catch((err) => {
+			return sendErrorMessage(
+				new AppError(400, "unsuccessful", "request body is inavlid"),
+				req,
+				res
+			);
+		});
+};
 
-    module.exports.addEmployees=addEmployees;
+module.exports.addEmployees = addEmployees;
