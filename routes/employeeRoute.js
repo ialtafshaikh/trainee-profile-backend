65502a1f9b7f832714ba3aaeb6eb2cb9c19e54f1
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-31 08:25:03 +0530

    routes modifed t0 support image upload

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,12 +1,21 @@
 const express = require("express");
+
 const { getAllemployees } = require("../controllers/employeeController");
 const { addEmployees } = require("../controllers/addEmployees");
 const { getEmployeebyId } = require("../controllers/getEmployeebyId");
 
+//middlewares
+const { uploadImage } = require("../middlewares/imageMiddlewares");
+
+const { multerUploads } = require("../config/multerConfig");
+const { cloudinaryConfig } = require("../config/cloudinaryConfig");
+
 const router = express.Router();
 
 router.route("/").get(getAllemployees);
-router.route("/addEmployee").post(addEmployees);
+router
+  .route("/addEmployee")
+  .post(cloudinaryConfig, multerUploads, uploadImage, addEmployees);
 router.route("/:empId").get(getEmployeebyId);
 
 module.exports = router;
