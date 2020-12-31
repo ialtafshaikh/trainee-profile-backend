7fbb115a5bf82f16d5a07659546bccc08208112b
ialtafshaikh <altaf.shaikh@contentstack.com>
2020-12-31 08:23:56 +0530

    multer config added

---
+++ config/multerConfig.js
@@ -0,0 +1,44 @@
+const multer = require("multer");
+const DatauriParser = require("datauri/parser");
+const path = require("path");
+const fs = require("fs");
+const dontenv = require("dotenv");
+dontenv.config({ path: "config.env" });
+
+let storage;
+let dataUri;
+const parser = new DatauriParser();
+
+if (process.env.STORAGE_TYPE == "cloud") {
+  storage = multer.memoryStorage();
+  dataUri = (req) =>
+    parser.format(
+      path.extname(req.file.originalname).toString(),
+      req.file.buffer
+    );
+} else {
+  dataUri = {};
+  storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      var directory = process.env.STORAGE;
+      if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory);
+      }
+      cb(null, directory);
+    },
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        file.fieldname +
+          "-" +
+          Date.now() +
+          "." +
+          file.originalname.split(".")[1]
+      );
+    },
+  });
+}
+
+const multerUploads = multer({ storage: storage }).single("imageUrl");
+
+module.exports = { multerUploads, dataUri };
